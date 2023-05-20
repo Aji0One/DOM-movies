fccaa4d29a9616af030ba415f95525691dc3639b
Aji0One <ajithkumarmspva@gmail.com>
2023-05-20 07:21:03 +0530

    altered files

--- main.js
+++ main.js
@@ -31,20 +31,22 @@ mydata.then((res) => {
 
 function searchData() {
   let target = document.getElementById("input").value;
-  let table = document.querySelector("table");
-  let tbody = document.querySelector("tbody");
-  let tr = document.querySelectorAll("tr");
-  let stbody = document.createElement("tbody");
-  for (let i = 0; i < tr.length; i++) {
-    let td = tr[i].querySelectorAll("td")[1];
-    if (td) {
-      let txtvalue = td.innerText || td.innerHTML;
-      // console.log(txtvalue);
-      tbody.innerHTML = "";
-      if (txtvalue == target) {
-        stbody.append(tr[i]);
+
+  mydata.then((resp) => {
+    tbody.innerHTML = "";
+    for (let i = 0; i < resp.length; i++) {
+      if (resp[i].year == target) {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const { title, year, description } = resp[i];
+        td1.innerHTML = title;
+        td2.innerHTML = year;
+        td3.innerHTML = description;
+        tr.append(td1, td2, td3);
+        tbody.append(tr);
       }
-      table.append(stbody);
     }
-  }
+  });
 }
